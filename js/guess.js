5a3d6bdb0ad7777a5a91b9e0d57bdcb6d29b0b82
Lenar Gasimov <lv4262122+github@gmail.com>
2020-11-14 16:16:01 +0300

    perf: moved files to js folder

---
+++ js/guess.js
@@ -0,0 +1,37 @@
+let answer = parseInt(Math.random() * 100);
+let tryCount = 0;
+let maxTryCount = 3;
+
+function readInt(){
+    let number = document.getElementById("userAnswer").value;
+    return parseInt(number);
+    // return +document.getElementById("userAnswer").value;
+}
+
+function write(text){
+    document.getElementById("info").innerHTML = text;
+}
+// скрывает элементы
+function hide(id){
+    document.getElementById(id).style.display = "none";
+}
+
+function guess(){
+    tryCount++;
+
+    let userAnswer = readInt();
+    if(userAnswer == answer){
+        write("<b>Поздравляю, вы угадали!</b>");
+        hide("button");
+        hide("userAnswer");
+        // 5 попыток
+    } else if(tryCount >= maxTryCount){
+        write("Вы проиграли<br>Правильный ответ: " + answer);
+        hide("button");
+        hide("userAnswer");
+    } else if(userAnswer > answer){
+        write("Вы ввели слишком большое число<br>Попробуйте еще раз. Введите число от 1 до 100");
+    } else if(userAnswer < answer){
+        write("Вы ввели слишком маленькое число<br>Попробуйте еще раз. Введите число от 1 до 100");				
+    }
+}
---
+++ js/puzzle.js
@@ -0,0 +1,22 @@
+let score = 0;
+
+function checkAnswer(inputId, answers){
+    let userAnswer = document.getElementById(inputId).value;
+    userAnswer = userAnswer.toLowerCase();
+    for(let i = 0; i < answers.length; i++){
+        if(userAnswer == answers[i]){
+            score++;
+            break;
+        }
+    }
+}
+
+function checkAnswers() {
+
+    checkAnswer("userAnswer1", ["сон", "сноведение"]);
+    checkAnswer("userAnswer2", ["морской", "укус акулы"]);
+    checkAnswer("userAnswer3", ["шахматный", "троянский"]);
+
+    alert("Вы отгадали " + score + " загадок");
+
+}
