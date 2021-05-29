8eed9cc1e9755e6eaec665d09d7ec91a2e47c39d
Lenar Gasimov <lv4262122+github@gmail.com>
2021-05-29 20:33:05 +0300

    build: add new my cv project

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+//Spotify
 const parseResponse = (res) => {
 const track = res.recenttracks.track[0];
 const artist = track.artist["#text"];
@@ -41,3 +42,34 @@ fetch(
 
 setInterval(getSong, 15 * 1000);
 getSong();
+
+//Clock
+function showTime(){
+    var date = new Date();
+    var h = date.getHours(); // 0 - 23
+    var m = date.getMinutes(); // 0 - 59
+    var s = date.getSeconds(); // 0 - 59
+    var session = "AM";
+
+    if(h == 0){
+        h = 12;
+    }
+
+    if(h > 12){
+        h = h - 12;
+        session = "PM";
+    }
+
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+
+    var time = h + ":" + m + ":" + s + " " + session;
+    document.getElementById("MyClockDisplay").innerText = time;
+    document.getElementById("MyClockDisplay").textContent = time;
+
+    setTimeout(showTime, 1000);
+
+}
+
+showTime();
