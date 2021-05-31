98e7ad5f2d1e8e0567439106f7f35f24306fde29
Lenar Gasimov <lv4262122+github@gmail.com>
2021-05-31 21:13:28 +0300

    fix: bug fix

--- js/index.js
+++ js/index.js
@@ -84,13 +84,4 @@ function myFunction() {
   }
 }
 
-//Sertificate
-function myFunction() {
-  var x = document.getElementById("myDIV");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-}
 
