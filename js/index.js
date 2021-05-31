25a987ca60cf3c12870930b0a49957282642a41f
Lenar Gasimov <lv4262122+github@gmail.com>
2021-05-31 20:35:02 +0300

    feat: add sertificate

--- js/index.js
+++ js/index.js
@@ -83,3 +83,14 @@ function myFunction() {
     x.className = "topnav";
   }
 }
+
+//Sertificate
+function myFunction() {
+  var x = document.getElementById("myDIV");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
