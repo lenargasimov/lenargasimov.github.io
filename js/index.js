be6827fc2a2a0f2b4cadb4f0dc1e8eb145c7fad4
Lenar Gasimov <lv4262122+github@gmail.com>
2021-10-06 22:26:41 +0300

    refactor: added button projects

--- js/index.js
+++ js/index.js
@@ -55,10 +55,26 @@ function myFunction() {
   }
 }
 
+//Carousel
+$(".carousel").owlCarousel({
+           margin: 20,
+           loop: true,
+           autoplay: true,
+           autoplayTimeout: 2000,
+           autoplayHoverPause: true,
+           responsive: {
+             0:{
+               items:1,
+               nav: false
+             },
+             600:{
+               items:2,
+               nav: false
+             },
+             1000:{
+               items:3,
+               nav: false
+             }
+           }
+         });
 
-$(".slider").owlCarousel({
-loop: true,
-autoplay: true,
-autoplayTimeout: 2000, //2000ms = 2s;
-autoplayHoverPause: true,
-});
